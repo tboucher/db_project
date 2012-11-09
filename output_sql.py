137b9c6ed2d14732ae56dd0fad9d868bef5cee2f
worker <worker@worker-VirtualBox.(none)>
2012-11-09 02:06:10 -0500

    - fixed a few more syntax errors - fixed a over counting bug

--- output_sql.py
+++ output_sql.py
@@ -9,21 +9,24 @@ for rows in clusters_reader:
 output = open('sql_stuff.sql', 'w')
 SSS = "(nextval('id_seq'), 'multipoint (" + "%s 0, " * 29 + "%s 0)', %s)"
 
-def do_write(features, cur, max):
+def do_write(features, cur, max, TO_WRITE_OUT):
     tu = array_to_tuple(features)
-    for i in features:
-        output.write(SSS % tu)
-        if cur < max-1:
-            output.write(",\n")
+    TO_WRITE_OUT += (SSS % tu)
+    if cur < max-1:
+        TO_WRITE_OUT += ",\n"
+
+    return TO_WRITE_OUT
 
 def write_all(all_the_features):
+    TO_WRITE_OUT = ""
     output.write("create sequence id_seq start 1;\n")
     output.write("CREATE TABLE sample_mpoints (id integer, features geometry, label integer);\n")
     output.write("INSERT INTO sample_mpoints (id, features, label) VALUES\n")
     l = len(all_the_features)
     for i in range(l):
-        do_write(all_the_features[i], i, l)
+        TO_WRITE_OUT = do_write(all_the_features[i], i, l, TO_WRITE_OUT)
 
+    output.write(TO_WRITE_OUT)
     output.write(";\n")
     output.close()
 
